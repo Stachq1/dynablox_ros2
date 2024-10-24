bb32453d87e88ca795aeb36a40f72b8fcb8759e8
Stachq1 <stanislawpiasecki@protonmail.ch>
2024-10-24 17:37:55 +0200

    Add more functionality to kindr_msg.h

--- minkindr/include/minkindr/kindr_msg.h
+++ minkindr/include/minkindr/kindr_msg.h
@@ -21,11 +21,21 @@ void vectorEigenToMsg(const Eigen::Vector3d &e, geometry_msgs::msg::Vector3 &m) 
   m.z = e(2);
 }
 
+void vectorMsgToEigen(const geometry_msgs::msg::Vector3 &m, Eigen::Vector3d &e) {
+  e(0) = m.x;
+  e(1) = m.y;
+  e(2) = m.z;
+}
+
 void quaternionEigenToMsg(const Eigen::Quaterniond &e, geometry_msgs::msg::Quaternion &m) {
   m.x = e.x();
   m.y = e.y();
   m.z = e.z();
   m.w = e.w();
+}
+
+void quaternionMsgToEigen(const geometry_msgs::msg::Quaternion &m, Eigen::Quaterniond &e) {
+  e = Eigen::Quaterniond(m.w, m.x, m.y, m.z);
 }
 
 // A wrapper for the relevant functions in eigen_conversions.
@@ -46,6 +56,25 @@ void quaternionKindrToMsg(
 }
 
 template <typename Scalar>
+void quaternionMsgToKindr(
+    const geometry_msgs::Quaternion& msg,
+    kindr::minimal::RotationQuaternionTemplate<Scalar>* kindr) {
+  CHECK_NOTNULL(kindr);
+  Eigen::Quaternion<Scalar> quat;
+  quaternionMsgToEigen(msg, quat);
+  *kindr = kindr::minimal::RotationQuaternionTemplate<Scalar>(quat);
+}
+
+template <typename Scalar>
+void quaternionMsgToKindr(
+    const geometry_msgs::Quaternion& msg, Eigen::Quaternion<Scalar>* kindr) {
+  CHECK_NOTNULL(kindr);
+  Eigen::Quaternion<double> kindr_double;
+  quaternionMsgToEigen(msg, kindr_double);
+  *kindr = kindr_double.cast<Scalar>();
+}
+
+template <typename Scalar>
 void vectorKindrToMsg(
     const Eigen::Matrix<Scalar, 3, 1>& kindr, geometry_msgs::msg::Vector3* msg) {
   CHECK_NOTNULL(msg);
@@ -62,6 +91,21 @@ void transformKindrToMsg(
   quaternionKindrToMsg(kindr.getRotation(), &msg->rotation);
 }
 
-}  // namespace tf
+template <typename Scalar>
+void transformMsgToKindr(
+    const geometry_msgs::msg::Transform& msg,
+    kindr::minimal::QuatTransformationTemplate<Scalar>* kindr) {
+  CHECK_NOTNULL(kindr);
+  Eigen::Matrix<Scalar, 3, 1> position;
+  Eigen::Quaternion<Scalar> rotation;
+
+  quaternionMsgToKindr(msg.rotation, &rotation);
+  vectorMsgToKindr(msg.translation, &position);
+
+  *kindr =
+      kindr::minimal::QuatTransformationTemplate<Scalar>(rotation, position);
+}
+
+}  // namespace tf2
 
 #endif  // MINKINDR_CONVERSIONS_KINDR_MSG_H
