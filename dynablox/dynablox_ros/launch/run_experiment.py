86d4f7a153590081f41ab625adbfc9336da8ae5b
Stachq1 <stanislawpiasecki@protonmail.ch>
2024-11-26 17:57:01 +0100

    Fix loading of parameters from config

--- dynablox/dynablox_ros/launch/run_experiment.py
+++ dynablox/dynablox_ros/launch/run_experiment.py
@@ -9,55 +9,14 @@ import os
 
 def generate_launch_description():
   # Declare the arguments
-  bag_file_arg = DeclareLaunchArgument(
-    'bag_file',
-    default_value='/root/rosbags/indoor/indoor.db3',
-    description='Full path to the bag file to play'
-  )
-
-  player_rate_arg = DeclareLaunchArgument(
-    'player_rate',
-    default_value='1',
-    description='Real time rate of bag being played'
-  )
-
-  evaluate_arg = DeclareLaunchArgument(
-    'evaluate',
-    default_value='false',
-    description='Whether to save evaluation data'
-  )
-
-  eval_output_path_arg = DeclareLaunchArgument(
-    'eval_output_path',
-    default_value='/root/dynablox_output',
-    description='Where to save evaluation data'
-  )
-
-  ground_truth_file_arg = DeclareLaunchArgument(
-    'ground_truth_file',
-    default_value='/root/data/DOALS/hauptgebaeude/sequence_1/indices.csv',
-    description='GT data file. Currently supports DOALS'
-  )
-
-  config_file_arg = DeclareLaunchArgument(
-    'config_file',
-    default_value='motion_detector/default.yaml',
-    description='Configuration of Dynablox'
-  )
-
-  visualize_arg = DeclareLaunchArgument(
-    'visualize',
-    default_value='true',
-    description='Whether to display RVIZ visualizations'
-  )
+  config_file = os.path.join(get_package_share_directory('dynablox_ros'), 'config', 'motion_detector', 'default.yaml')
 
   # Include play_dynablox_data.launch
   play_dynablox_data_launch = IncludeLaunchDescription(
     PythonLaunchDescriptionSource(
       os.path.join(get_package_share_directory('dynablox_ros'), 'launch', 'play_dynablox_data.py')
     ),
-    launch_arguments={'bag_file': LaunchConfiguration('bag_file'),
-                      'player_rate': LaunchConfiguration('player_rate')}.items()
+    launch_arguments={'bag_file': '/root/rosbags/indoor/indoor.db3', 'player_rate': '1'}.items()
   )
 
   motion_detector_node = Node(
@@ -66,14 +25,7 @@ def generate_launch_description():
     name='motion_detector_node',
     output='screen',
     arguments=['--alsologtostderr'],
-    parameters=[
-      {
-        'evaluation/ground_truth/file_path': LaunchConfiguration('ground_truth_file'),
-        'evaluation/output_directory': LaunchConfiguration('eval_output_path'),
-        'evaluate': LaunchConfiguration('evaluate'),
-        'config_file': LaunchConfiguration('config_file')
-      }
-    ]
+    parameters=[config_file]
   )
 
   rviz_node = Node(
@@ -83,13 +35,6 @@ def generate_launch_description():
   )
 
   return LaunchDescription([
-    bag_file_arg,
-    player_rate_arg,
-    evaluate_arg,
-    eval_output_path_arg,
-    ground_truth_file_arg,
-    config_file_arg,
-    visualize_arg,
     play_dynablox_data_launch,
     motion_detector_node,
     rviz_node
